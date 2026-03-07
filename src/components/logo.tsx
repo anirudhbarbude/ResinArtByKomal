6c5e6958211b3e3e5bee5af7133aee1bae5d016a
Anirudh Barbude <asbarbude@gmail.com>
2026-03-07 14:19:55 +0000

    add this logo in centre  artistic resin by komal make sure it merge eith

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,18 @@
 import { cn } from "@/lib/utils";
 import type { HTMLAttributes } from "react";
+import Image from "next/image";
 
 export function Logo({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div
-      className={cn(
-        "font-logo text-4xl text-primary",
-        className
-      )}
-      {...props}
-    >
-      AK
+    <div className={cn(className)} {...props}>
+      <Image
+        src="https://i.imgur.com/iNpV4QY.png"
+        alt="Artistic Resin by Komal logo"
+        width={103}
+        height={83}
+        className="dark:invert w-auto h-12"
+        priority
+      />
     </div>
   );
 }
